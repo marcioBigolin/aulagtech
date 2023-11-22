f15e153177b3b183ec7f8dcc2fe1e8aab8d9c328
Marcio Bigolin <marcio.bigolinn@gmail.com>
2023-11-22 09:46:04 -0300

    aa

--- aula.py
+++ aula.py
@@ -5,4 +5,6 @@ st.write("meu teste alterado")
 dado = pd.read_csv("data.csv")
 
 st.write(dado)
-estatisticas_basicas = df['aa_num'].describe()
+estatisticas_basicas = dado['aa_num'].describe()
+
+st.write(dado)
