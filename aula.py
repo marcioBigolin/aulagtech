f4de428ddfba1185ffecf57fc91e2722d39f0d45
Marcio Bigolin <marcio.bigolinn@gmail.com>
2023-11-22 07:11:12 -0300

    Apenas senha

--- aula.py
+++ aula.py
@@ -7,10 +7,10 @@ from sqlalchemy import create_engine
 # Recebe os parâmetros via GET enquanto sem criptografia mandando direto (usar bearertok)
 params = st.experimental_get_query_params()
 # Obtém o valor do parâmetro 'variavel' da URL
-bd = params.get('banco', ['SEM_DADOS'])[0]
+senha = params.get('senha', ['SEM_DADOS'])[0]
 
 #criando conexão com o banco de dados
-conn = create_engine(bd)
+conn = create_engine(f"postgresql://revisao_data:{senha}@revisao_data.postgresql.dbaas.com.br:5432/revisao_data")
 
 
 #montando a tela 
@@ -18,6 +18,8 @@ st.set_page_config(page_title="Aula de low code GtechEDU", layout="wide")
 st.subheader("Explore os dados utilizando Inteligência artificial")
 st.write("Escrever algo")
 
+#consultando o banco de dados (conhecimento de SQL)
 sql_query =  pd.read_sql_query (f"SELECT * FROM moodle_marcio2.fato_join;", con=conn)
 
 df = pd.DataFrame(sql_query, columns = ['titulo', 'nome_completo', 'coh_frazier', 'coh_brunet', 'data_entrega'])
+
