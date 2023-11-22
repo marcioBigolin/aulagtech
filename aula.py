f836c94ca8f335bab5bdc0374ace1cef8188c19f
Marcio Bigolin <marcio.bigolinn@gmail.com>
2023-11-22 07:23:27 -0300

    f

--- aula.py
+++ aula.py
@@ -8,14 +8,14 @@ from sqlalchemy import create_engine
 params = st.experimental_get_query_params()
 # Obtém o valor do parâmetro 'variavel' da URL
 senha = params.get('senha', ['SEM_DADOS'])[0]
+st.write(senha)
+
 
 #criando conexão com o banco de dados
 conn = create_engine(f"postgresql://revisao_data:{senha}@revisao_data.postgresql.dbaas.com.br:5432/revisao_data")
 st.write(f"postgresql://revisao_data:{senha}@revisao_data.postgresql.dbaas.com.br:5432/revisao_data")
 
 #montando a tela 
-st.set_page_config(page_title="Aula de low code GtechEDU", layout="wide")
-st.subheader("Explore os dados utilizando Inteligência artificial")
 st.write("Escrever algo")
 
 #consultando o banco de dados (conhecimento de SQL)
@@ -24,3 +24,4 @@ sql_query =  pd.read_sql_query (f"SELECT * FROM moodle_marcio2.fato_join;", con=
 df = pd.DataFrame(sql_query, columns = ['titulo', 'nome_completo', 'coh_frazier', 'coh_brunet', 'data_entrega'])
 
 st.dataframe(df)
+
